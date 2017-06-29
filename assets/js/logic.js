8b4aa90f77b6aaebb44cf054188683271866e6b9
Alice Yu <ayu2013@gmail.com>
2017-06-29 13:43:13 -0500

    Made stuff better and prettier

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -19,135 +19,148 @@ const categories = {
     "Entertainment: Video Games": 15,
 };
 
+const difficulties = ["easy", "medium", "hard"];
+
 $(document).ready(function () {
-
-        function init() {
-            currTime = 30;
-            numOfQuestions = 0;
-            numberOfRightAnswers = 0;
-            numberOfWrongAnswers = 0;
-
-            for (let prop in categories) {
-                $("#gameText").text("Pick a category!");
-                $(".gameContainer").append($("<button>").text(prop).addClass("linedButton category").val(categories[prop]).click(function () {
-                    initGame($(this).val());
-                }));
-            }
-        }
-
-        function initGame(category) {
-            $(".mainTextHolder").append($("<div>").text("Question: "), $("<div>").text("Time remaining: "), $("<div>").text("Score: "));
-            $("#gameText").text("");
-            $(".category").remove();
-
-            $.get("https://opentdb.com/api.php?amount=10&category=" + category, function(response){
-                console.log("Success. Retrieved ", response);
-            })
-                .done(function(response){
-                    currQuestions = response.results;
-                    runTrivia();
-                })
-                .fail(function(response){
-                    console.log("Error ", response.response_code);
-                })
-        }
-
-        //function fetchQuestions(category) {
-            // let request = new Request("https://opentdb.com/api.php?amount=10&category=18");
-            //
-            // fetch(request).then(function(response){
-            //     return response.json();
-            // }).then(function(response){
-            //     currQuestions = response.results;
-            //     console.log(currQuestions);
-            //     //runTrivia();
-            // }).catch(function(err){
-            //     console.log("Fetch Error", err);
-            // });
-        //}
-
-        function parseAnswers(answers) {
-            let returnArray = [];
-            for (let i = 0; i < answers.length; i++) {
-                returnArray.push(parseHTML(answers[i]));
-            }
-            return returnArray;
-        }
-
-        function parseHTML(HTML) {
-            return $("<textarea/>").html(HTML).text();
-        }
-
-        function displayQuestion(question) {
-            $(".question, .answers").remove();
-            let currQuestion = parseHTML(question["question"]);
-            let incorrectAnswers = parseAnswers(question["incorrect_answers"]);
-            correctAnswer = parseHTML(question["correct_answer"]);
-            let answers = incorrectAnswers.slice(0);
-            answers.push(correctAnswer);
-
-            $(questionDiv).prepend($("<div>").text(currQuestion).addClass("question"));
-
-            for (let i = 0; i < answers.length; i++) {
-                $(answersDiv).append($("<button>").text(parseHTML(answers[i])).addClass("answers linedButton").val(answers[i]));
-            }
-
-            $(".answers").click(function (event) {
-                checkAnswer(event.target.value);
-            });
-        }
-
-        function checkAnswer(userValue) {
-            console.log(userValue);
-            if (userValue === correctAnswer) {
-                console.log("correct answer");
-                numberOfRightAnswers++;
-            } else if (userValue === "timeout") {
-                console.log("time out");
-                numberOfWrongAnswers++;
-            } else {
-                console.log("incorrect answer");
-                numberOfWrongAnswers++;
-            }
-            numOfQuestions++;
-            clearInterval(timeoutID);
-            clearInterval(questionTimer);
-            currTime = 30;
-            runTrivia();
-        }
-
-        function setGameText() {
-            $("#questionIndexDisplay").text(" " + numOfQuestions + " out of 10");
-            $("#score").text(" " + numberOfRightAnswers + "  out of " + (numOfQuestions));
-        }
-
-        function runTrivia() {
-            setGameText();
-            if (numOfQuestions < 10) {
-                displayQuestion(currQuestions[numOfQuestions]);
-                timeoutID = window.setTimeout(checkAnswer, 30000, "timeout");
-                timer();
-            } else {
-                gameEnd();
-            }
-        }
-
-        function timer() {
-            currTime--;
-            $("#timer").text(" " + currTime);
-            questionTimer = setTimeout(timer, 1000);
-        }
-
-        function gameEnd() {
-            $(".question, .answers").remove();
-            $("#gameText").html($("<div>").addClass("final").text("Final score: " + numberOfRightAnswers + "  out of " + (numOfQuestions)))
-        }
-
-        //fetchQuestions();
         init();
     }
 );
 
+function init() {
+    currTime = 30;
+    numOfQuestions = 0;
+    numberOfRightAnswers = 0;
+    numberOfWrongAnswers = 0;
+    $(".gameContainer").prepend($("<span>Pick a category!</span>").addClass("initialText"));
+
+    for (let prop in categories) {
+        $(".gameContainer").append($("<button>").text(prop).addClass("linedButton category").val(categories[prop]).click(function () {
+            $(".category").animate({
+                "animation": "fadeOutDown 3s ease",
+            });
+            difficulty($(this).val());
+        }));
+    }
+}
+
+function difficulty(category) {
+    $(".category").remove();
+    $(".initialText").text("Pick a difficulty!");
+
+    for (let i = 0; i < difficulties.length; i++) {
+        $(".gameContainer").append($("<button>").text(difficulties[i]).addClass("linedButton difficulty").val(difficulties[i]).click(function () {
+            initGame(category, $(this).val());
+        }));
+    }
+}
+
+function initGame(category, difficulty) {
+    $(".initialText").remove();
+    $(".difficulty").remove();
+    $(".mainTextHolder").append($("<div>").text("Question: "), $("<div>").text("Time remaining: "), $("<div>").text("Score: "));
 
 
+    $.get("https://opentdb.com/api.php?amount=10&category=" + category + "&difficulty=" + difficulty, function (response) {
+        console.log("Success. Retrieved ", response);
+    })
+        .done(function (response) {
+            currQuestions = response.results;
+            runTrivia();
+        })
+        .fail(function (response) {
+            console.log("Error ", response.response_code);
+            initGame(category, difficulty);
+        })
+}
 
+function parseAnswers(answers) {
+    let returnArray = [];
+    for (let i = 0; i < answers.length; i++) {
+        returnArray.push(parseHTML(answers[i]));
+    }
+    return returnArray;
+}
+
+function parseHTML(HTML) {
+    return $("<textarea/>").html(HTML).text();
+}
+
+function shuffleArray(array) {
+    let n = array.length;
+    let t;
+    let i;
+
+    while (n) {
+        i = Math.random() * n-- | 0;
+        t = array[n];
+        array[n] = array[i];
+        array[i] = t;
+    }
+    return array;
+}
+
+function displayQuestion(question) {
+    $(".question, .answers").remove();
+    let currQuestion = parseHTML(question["question"]);
+    let incorrectAnswers = parseAnswers(question["incorrect_answers"]);
+    correctAnswer = parseHTML(question["correct_answer"]);
+    let answers = incorrectAnswers.slice(0);
+    answers.push(correctAnswer);
+    answers = shuffleArray(answers);
+
+    $(questionDiv).prepend($("<div>").text(currQuestion).addClass("question"));
+
+    for (let i = 0; i < answers.length; i++) {
+        $(answersDiv).append($("<button>").text(parseHTML(answers[i])).addClass("answers linedButton").val(answers[i]));
+    }
+
+    $(".answers").click(function (event) {
+        checkAnswer(event.target.value);
+    });
+}
+
+function checkAnswer(userValue) {
+    if (userValue === correctAnswer) {
+        numberOfRightAnswers++;
+    } else if (userValue === "timeout") {
+        numberOfWrongAnswers++;
+    } else {
+        numberOfWrongAnswers++;
+    }
+    numOfQuestions++;
+    clearInterval(timeoutID);
+    clearInterval(questionTimer);
+    currTime = 30;
+    runTrivia();
+}
+
+function setGameText() {
+    $("#questionIndexDisplay").text(" " + numOfQuestions + " out of 10");
+    $("#score").text(" " + numberOfRightAnswers + "  out of " + (numOfQuestions));
+}
+
+function runTrivia() {
+    setGameText();
+    if (numOfQuestions < 10) {
+        displayQuestion(currQuestions[numOfQuestions]);
+        timeoutID = window.setTimeout(checkAnswer, 30000, "timeout");
+        timer();
+    } else {
+        gameEnd();
+    }
+}
+
+function timer() {
+    currTime--;
+    $("#timer").text(" " + currTime);
+    questionTimer = setTimeout(timer, 1000);
+}
+
+function gameEnd() {
+    $(".question, .answers").remove();
+    $("#gameText").html($("<div>").addClass("final").text("Final score: " + numberOfRightAnswers + "  out of " + (numOfQuestions)));
+    $("#gameText").append($("<button>").addClass("linedButton").text("Play again!").click(function () {
+        init();
+        $(this).remove();
+    }));
+}
